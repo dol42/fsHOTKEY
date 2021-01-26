0704f914fe0562dd91146d8bf6eb459cf2a4eba6
dol42 <kent2499@naver.com>
2021-01-26 23:26:52 +0900

    hotkey ver0.1 + exefile for windows

--- fsstrans.py
+++ fsstrans.py
@@ -4,29 +4,33 @@
 #https://doljokilab.tistory.com/
 
 #키 후킹을 위한 파이썬 라이브러리 임포트 
-from pynput import keyboard
-from pynput.keyboard import Key, Controller
-kp = Controller()
+import pynput
+import os
+
+kp = pynput.keyboard.Controller()
 print("fs솔루션, delete키를 spacebar로 대체하는 프로그램입니다.(ver 0.1)")
 print("사용시 발생하는 문제는 개발자가 따로 책임지지 않습니다.")
 print("프로그램 실행중, 'esc'누르면 종료됩니다.")
 print("소스코드 공개 : https://github.com/dol42/fsHOTKEY/blob/master/fsstrans.py ")
+
 
 def on_press(key):
     return
 
 #중복키 방지를 위해 키보드에서 뗐을경우 실행하는 함수
 def on_release(key):
-    if key == keyboard.Key.esc:
+    if key == pynput.keyboard.Key.esc:
         print("프로그램 종료")
         return False
-    elif key == keyboard.Key.space:
-        kp.press(keyboard.Key.delete)
-        kp.release(keyboard.Key.delete)
+    elif key == pynput.keyboard.Key.space:
+        kp.press(pynput.keyboard.Key.delete)
+        kp.release(pynput.keyboard.Key.delete)
 
-with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
+with pynput.keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
     listener.join()
-# 키 후킹용 리스너 실행
-listener = keyboard.Listener(on_press=on_press,on_release=on_release)
-listener.start()
 
+
+# 키 후킹용 리스너 실행
+listener = pynput.keyboard.Listener(on_press=on_press,on_release=on_release)
+listener.start()
+os.system("pause")
